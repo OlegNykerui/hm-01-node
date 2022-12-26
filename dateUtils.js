da5857cebe43d8193b111a593c597960ff6cbfca
Oleg Nykerui <nykeruyoleg96@gmail.com>
2022-12-26 16:41:11 +0200

    delete utils

--- dateUtils.js
+++
@@ -1,9 +0,0 @@
-// function getCurrentDate() {
-//   const date = Date.now();
-//   const today = new Date(date);
-//   return today.toLocaleDateString();
-// }
-
-// module.exports = {
-//   getCurrentDate,
-// };
